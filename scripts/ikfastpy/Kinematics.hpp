3f362975c3ef91a771b0cc70e012644bcdc452c3
nhcha6 <nhcha6@student.monash.edu>
2021-10-07 14:46:09 +1100

    Delete Kinematics.hpp

--- scripts/ikfastpy/Kinematics.hpp
+++
@@ -1,9 +0,0 @@
-namespace robots { 
-    class Kinematics { 
-        public: int num_of_joints, num_free_parameters; 
-        Kinematics(); 
-        ~Kinematics(); 
-        std::vector<float> forward(std::vector<float> joint_config);
-        std::vector<float> inverse(std::vector<float> ee_pose);
-    }; 
-}
